2b684b710df69dd993a5f4a7a3814973e940c3af
magosil86 <magosil86@gmail.com>
2017-06-06 08:34:40 +0100

    update citation

--- R/util_draw_table.R
+++ R/util_draw_table.R
@@ -17,23 +17,23 @@
 #' @param heading A string. Table title.
 #' @param ... Further arguments to control the gtable.
 #'
-#'  @examples
-#'  # note: not exported hence examples are not run
-#'  library(gridExtra)
+#' @examples
+#' # note: not exported hence examples are not run
+#' library(gridExtra)
 #'
-#'  # Table of iris values
-#'  iris_dframe <- head(iris)
-#'  title_iris_dframe <- paste("Table: Length and width measurements (cm) of sepals ",
+#' # Table of iris values
+#' iris_dframe <- head(iris)
+#' title_iris_dframe <- paste("Table: Length and width measurements (cm) of sepals ",
 #'                        "and petals, for 50 flowers from 3 species of iris ", 
 #'                        "(setosa, versicolor, and virginica). ", sep="")
-#'  table_influential_studies <-  draw_table(body = iris_dframe, heading = title_iris_dframe)
+#' table_influential_studies <-  draw_table(body = iris_dframe, heading = title_iris_dframe)
 #' 
-#'  # Table of mtcars values 
-#'  mtcars_dframe <- head(mtcars)
-#'  title_mtcars_dframe <- paste("Table: Motor Trend US magazine (1974) automobile ",
+#' # Table of mtcars values 
+#' mtcars_dframe <- head(mtcars)
+#' title_mtcars_dframe <- paste("Table: Motor Trend US magazine (1974) automobile ",
 #'                         "statistics for fuel consumption, automobile ", 
 #'                         "design and performance. ", sep="")
-#'  table_influential_studies <-  draw_table(body = mtcars_dframe, 
+#' table_influential_studies <-  draw_table(body = mtcars_dframe, 
 #'                                           heading = title_mtcars_dframe)
 
 #' # @export
